806406b182c4bfcacf38b20448a3782281c59d91
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 21:11:05 +0200

    added router

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
-'use strict';
-var SwaggerExpress = require('swagger-express-mw');
+
+var http = require("http");
 var express = require('express');
 var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
-var config = {
-    appRoot: __dirname // required config
-};
-
+var routes = require('./routes/index');
 app.set('port', (process.env.PORT || 10010));
 
 app.use(express.static(__dirname + '/public'));
-
+app.use('/', routes);
 //app.get('/', function (request, response) {
 //    response.render('index');
 //});
@@ -20,35 +14,3 @@ app.use(express.static(__dirname + '/public'));
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
-
-SwaggerExpress.create(config, function (err, swaggerExpress) {
-    if (err) { throw err; }
-
-    // install middleware
-    swaggerExpress.register(app);
-
-    //var port = process.env.PORT || 10010;
-    //app.listen(port);
-
-    if (swaggerExpress.runner.swagger.paths['/hello']) {
-        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-    }
-});
-
-module.exports = app; // for testing
-
-//var http = require("http");
-//var express = require('express');
-//var app = express();
-
-//app.set('port', (process.env.PORT || 10010));
-
-//app.use(express.static(__dirname + '/public'));
-
-//app.get('/', function (request, response) {
-//    response.render('index');
-//});
-
-//app.listen(app.get('port'), function () {
-//    console.log('Node app is running on port', app.get('port'));
-//});
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+﻿var express = require('express');
+var router = express.Router();
+var personality = require('../api/controllers/personality');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Traits api' });
+});
+
+router.get('/api/personality', function (req, res) {
+    //res.send({ test: 'Hello handsome: ' + req.query.test });
+    personality.get_all_personalities(req, res, function () {
+        console.log('Get all personalities');
+    });
+})
+
+//router.get('/analytics/v0_1_0/poweranalysisday/:resultsid', function (req, res) {
+//    var id = req.params.resultsid;
+//    //res.send({ test: 'Get analytics - test: ' + req.query.test + ', id: ' + id }); 
+//    analysis.get_poweranalysisday_result(req, res, function () {
+//        console.log('GET');
+//    });
+//});
+
+//router.post('/analytics/testing/:id', function (req, res) {
+//    analysis.testing(req, res, function () {
+//        console.log('testing');
+//    });
+//});
+
+module.exports = router;
