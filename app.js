d67c284d8115603286154e75c63929deec6b13b7
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 21:26:14 +0200

    update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var router = express.Router();
 var app = express();
 var routes = require('./routes/index');
 app.set('port', (process.env.PORT || 10010));
 
 app.use(express.static(__dirname + '/public'));
-app.use('/', routes);
-//app.get('/', function (request, response) {
-//    response.render('index');
-//});
+//app.use('/', routes);
+app.get('/', function (request, response) {
+    response.render('index');
+});
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
