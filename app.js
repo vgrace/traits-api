47b60728abe1843393948bb9cc80bfacf096b4d9
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 21:42:19 +0200

    updated

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-/// <reference path="routes/index.js" />
+var SwaggerExpress = require('swagger-express-mw');
 var express = require('express');
 var app = express();
+module.exports = app; // for testing
+
 //var routes = require('./routes/index.js');
-app.set('port', (process.env.PORT || 10010));
+//app.set('port', (process.env.PORT || 10010));
 
 app.use(express.static(__dirname + '/public'));
 //app.use('/', routes);
@@ -10,23 +12,26 @@ app.get('/', function (request, response) {
     response.render('index');
 });
 
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
-
-//var express = require('express');
-//var app = express();
-
-//app.set('port', (process.env.PORT || 5000));
-
-//app.use(express.static(__dirname + '/public'));
-
-//app.get('/', function (request, response) {
-//    response.render('index');
-//});
-
 //app.listen(app.get('port'), function () {
 //    console.log('Node app is running on port', app.get('port'));
 //});
+
+var config = {
+    appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+    if (err) { throw err; }
+
+    // install middleware
+    swaggerExpress.register(app);
+
+    var port = process.env.PORT || 10010;
+    app.listen(port);
+
+    if (swaggerExpress.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    }
+});
 
 module.exports = app;
