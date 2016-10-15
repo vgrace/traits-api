0a45a53680b4b1734c09168f39740e2816a1f576
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 13:20:46 +0200

    Not using SwaggerExpress.create

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 var SwaggerExpress = require('swagger-express-mw');
 
 var app = express();
+app.set('port', (process.env.PORT || 10010));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,26 +23,38 @@ app.use(cookieParser());
 //set the public static resource folder (makes the public folder public and accessible from the browser)
 app.use(express.static(__dirname + "/public"));
 
+
+app.get('/', function (request, response) {
+    response.render('pages/index');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
+
+
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
 
-  // install middleware
-  swaggerExpress.register(app);
+//SwaggerExpress.create(config, function(err, swaggerExpress) {
+//  if (err) { throw err; }
 
-  var port = process.env.PORT || 10010;
-  app.set('port', port);
-  app.listen(port);
+//  // install middleware
+//  swaggerExpress.register(app);
 
-  if (swaggerExpress.runner.swagger.paths['/hello']) {
-    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-  }
+//  var port = process.env.PORT || 10010;
+//  app.set('port', port);
+//  app.listen(port);
+
+//  if (swaggerExpress.runner.swagger.paths['/hello']) {
+//    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+//  }
     
-  console.log("Listening on port: " + port); 
-});
+//  console.log("Listening on port: " + port); 
+//});
 
