51d922753ba2446585bb199c3430e4001cbfa799
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 21:01:59 +0200

    updated app.js

--- app.js
+++ app.js
@@ -9,14 +9,26 @@ var config = {
     appRoot: __dirname // required config
 };
 
+app.set('port', (process.env.PORT || 10010));
+
+app.use(express.static(__dirname + '/public'));
+
+//app.get('/', function (request, response) {
+//    response.render('index');
+//});
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
+
 SwaggerExpress.create(config, function (err, swaggerExpress) {
     if (err) { throw err; }
 
     // install middleware
     swaggerExpress.register(app);
 
-    var port = process.env.PORT || 10010;
-    app.listen(port);
+    //var port = process.env.PORT || 10010;
+    //app.listen(port);
 
     if (swaggerExpress.runner.swagger.paths['/hello']) {
         console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
