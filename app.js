7fb2347f245b508a2bbc48acfbe26ee284aaacc3
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 21:38:52 +0200

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 /// <reference path="routes/index.js" />
 var express = require('express');
 var app = express();
-var routes = require('./routes/index.js');
+//var routes = require('./routes/index.js');
 app.set('port', (process.env.PORT || 10010));
 
 app.use(express.static(__dirname + '/public'));
