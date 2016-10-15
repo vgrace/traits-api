df8bd1c868628eb767e7f9f8cb79d7ec734e6d26
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 21:12:47 +0200

    updated app.js

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.set('port', (process.env.PORT || 10010));
 
 app.use(express.static(__dirname + '/public'));
 app.use('/', routes);
-//app.get('/', function (request, response) {
-//    response.render('index');
-//});
+app.get('/', function (request, response) {
+    response.render('index');
+});
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
