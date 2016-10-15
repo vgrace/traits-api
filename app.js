87246564340358dfc7ce9a28309bf2950b8dac1a
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 21:15:03 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-
-var http = require("http");
 var express = require('express');
 var app = express();
-var routes = require('./routes/index');
+//var routes = require('./routes/index');
 app.set('port', (process.env.PORT || 10010));
 
 app.use(express.static(__dirname + '/public'));
-app.use('/', routes);
+//app.use('/', routes);
 app.get('/', function (request, response) {
     response.render('index');
 });
@@ -14,3 +12,18 @@ app.get('/', function (request, response) {
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
 });
+
+//var express = require('express');
+//var app = express();
+
+//app.set('port', (process.env.PORT || 5000));
+
+//app.use(express.static(__dirname + '/public'));
+
+//app.get('/', function (request, response) {
+//    response.render('index');
+//});
+
+//app.listen(app.get('port'), function () {
+//    console.log('Node app is running on port', app.get('port'));
+//});
