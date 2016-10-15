811b3b7640eca43c89a29bba0a15fc0e337131fa
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-15 20:58:14 +0200

    updated app.js to work on postman

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 'use strict';
-var http = require("http");
+var SwaggerExpress = require('swagger-express-mw');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 10010));
-
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (request, response) {
-    response.render('index');
+var config = {
+    appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+    if (err) { throw err; }
+
+    // install middleware
+    swaggerExpress.register(app);
+
+    var port = process.env.PORT || 10010;
+    app.listen(port);
+
+    if (swaggerExpress.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    }
 });
 
-app.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
+module.exports = app; // for testing
+
+//var http = require("http");
+//var express = require('express');
+//var app = express();
+
+//app.set('port', (process.env.PORT || 10010));
+
+//app.use(express.static(__dirname + '/public'));
+
+//app.get('/', function (request, response) {
+//    response.render('index');
+//});
+
+//app.listen(app.get('port'), function () {
+//    console.log('Node app is running on port', app.get('port'));
+//});
